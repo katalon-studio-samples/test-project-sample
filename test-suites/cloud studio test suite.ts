4a13ffd1bf8f10bb7fee33e319484b9fccfc5cbb
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-05-24 10:51:23 +0700

    created cloud studio test suite

---
+++ test-suites/cloud studio test suite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>cloud studio test suite</name>
+    <testSuiteGuid>ae5bd1b2-e78e-4bbb-a9ed-d9b151ac421a</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/cloud studio test case</testCaseId>
+        <guid>279636f4-95b3-46d3-a3a9-0da15dd52db2</guid>
+    </testCaseLink>
+</TestSuiteEntity>
