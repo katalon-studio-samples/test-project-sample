a90ed94b79992f1bf8c97f4c0c985a4707ce66d9
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-06-20 15:09:19 +0700

    commit

---
+++ test-suites/CS TS.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>CS TS</name>
+    <testSuiteGuid>cff77045-6dce-431a-9549-49257d74c28a</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/cloud studio test case</testCaseId>
+        <guid>8e162aa5-f0b5-446f-b064-74104ef41631</guid>
+    </testCaseLink>
+</TestSuiteEntity>
